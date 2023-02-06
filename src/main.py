15a42dadcd8cdb11915b43dfce06c5717b680fda
itsmdjamil <zcabjam@ucl.ac.uk>
2023-02-06 16:32:42 +0000

    Configure environment variables

--- src/main.py
+++ src/main.py
@@ -1,24 +1,31 @@
 import requests
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 
 def configure():
     load_dotenv()
 
-city = "London"
-url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={os.getenv('api_key')}"
+def main():
+    configure()
 
-# Send a request to the API and retrieve the response
-response = requests.get(url)
-print(response.json())
+    city = "London"
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={os.getenv('api_key')}"
+    print(url)
 
-# Check if the request was successful
-if response.status_code == 200:
-    data = response.json()
+    # Send a request to the API and retrieve the response
+    response = requests.get(url)
+    print(response.json())
 
-    temperature = data["main"]["temp"]
-    description = data["weather"][0]["description"]
-    
-    print(f"The current weather in {city} is {temperature}°C with {description}.")
-else:
-    print("Failed to retrieve weather information.")
+    # Check if the request was successful
+    if response.status_code == 200:
+        data = response.json()
+
+        temperature_C = data["main"]["temp"] - 273.15
+        description = data["weather"][0]["description"]
+        
+        print(f"The current weather in {city} is {temperature_C:.2f}°C with {description}.")
+    else:
+        print("Failed to retrieve weather information.")
+
+if __name__ == "__main__":
+    main()
